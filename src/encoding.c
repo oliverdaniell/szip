d58e13a90075ab23b69a919bcbf389b9e7c8d691
epourmal <epourmal@af834410-e117-0410-aeac-9316c7680557>
2004-03-16 16:14:27 +0000

    Purpose: Maintenance     [is this a bug fix? feature? ...] Description: Added support for new autoconf/ automake tools     [describe the bug, or describe the new feature, etc] Solution:     [details about the changes, algorithm, etc...]     [Please as detail as you can since your own explanation is     better than others guessing it from the code.] Platforms tested: arabica, eirene     [machines you have tested the changed version.  This is absolute     important.  Test it out on at least two or three different platforms     such as Big-endian-32bit (SUN/IRIX), little-endian-32(LINUX) and     64-bit (IRIX64/UNICOS/DEC-ALPHA) would be good.]
    git-svn-id: https://svn.hdfgroup.uiuc.edu/szip/trunk@36 af834410-e117-0410-aeac-9316c7680557

---
+++ src/encoding.c
@@ -0,0 +1,6 @@
+/***************************************/
+/* Set to 1 to allow szip encoding.    */
+/* Set to 0 to prohibit szip encoding. */
+/***************************************/
+int szip_allow_encoding = 1; 
+/*int szip_allow_encoding = 0; */
