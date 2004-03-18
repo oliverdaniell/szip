ed8a912a6bd64f1839e0e5d3b9e2686564eac632
epourmal <epourmal@af834410-e117-0410-aeac-9316c7680557>
2004-03-18 21:35:53 +0000

    Purpose:a Maintenance
    Description: We now define HAVE_ENCODING variable at configure
                 time and use it to disable/enable encoding
                 in the library.
    Platforms tested: colonelk
    
    
    git-svn-id: https://svn.hdfgroup.uiuc.edu/szip/trunk@43 af834410-e117-0410-aeac-9316c7680557

--- src/encoding.c
+++ src/encoding.c
@@ -2,11 +2,16 @@
 /* Set to 1 to allow szip encoding.    */
 /* Set to 0 to prohibit szip encoding. */
 /***************************************/
+#include "SZconfig.h"
+
+#ifndef HAVE_ENCODING
+#define REMOVE_SZIP_ENCODER 1 
+#endif
 
 #ifdef REMOVE_SZIP_ENCODER
 int szip_allow_encoding = 0; 
-static char * szip_endoder_status = "SZIP ENCODER DISABLED";
+static char * szip_encoder_status = "SZIP ENCODER DISABLED";
 #else
 int szip_allow_encoding = 1; 
-static char * szip_endoder_status = "SZIP ENCODER ENABLED";
+static char * szip_encoder_status = "SZIP ENCODER ENABLED";
 #endif
