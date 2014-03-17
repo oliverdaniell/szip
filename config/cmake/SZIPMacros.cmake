0dbeb5e4e518e301996f36f2aa3e3bb8467dd2d1
byrn <byrn@af834410-e117-0410-aeac-9316c7680557>
2014-03-17 20:31:04 +0000

    Bump up minimum version. KitWare recommendations: Change CMake commands to lower case.
    git-svn-id: https://svn.hdfgroup.uiuc.edu/szip/trunk@155 af834410-e117-0410-aeac-9316c7680557

--- config/cmake/SZIPMacros.cmake
+++ config/cmake/SZIPMacros.cmake
@@ -3,34 +3,34 @@ MACRO (SZIP_SET_LIB_OPTIONS libtarget libname libtype)
   # message (STATUS "${libname} libtype: ${libtype}")
   HDF_SET_LIB_OPTIONS (${libtarget} ${libname} ${libtype})
 
-  IF (${libtype} MATCHES "SHARED")
-    IF (WIN32)
-      SET (LIBSZIP_VERSION ${SZIP_PACKAGE_VERSION_MAJOR})
-    ELSE (WIN32)
-      SET (LIBSZIP_VERSION ${SZIP_PACKAGE_VERSION})
-    ENDIF (WIN32)
+  if (${libtype} MATCHES "SHARED")
+    if (WIN32)
+      set (LIBSZIP_VERSION ${SZIP_PACKAGE_VERSION_MAJOR})
+    else (WIN32)
+      set (LIBSZIP_VERSION ${SZIP_PACKAGE_VERSION})
+    endif (WIN32)
 
-    IF (NOT CYGWIN)
+    if (NOT CYGWIN)
       # This property causes shared libraries on Linux to have the full version
       # encoded into their final filename.  We disable this on Cygwin because
       # it causes cygszip-${SZIP_FULL_VERSION}.dll to be created when cygszip.dll
       # seems to be the default.
-      SET_TARGET_PROPERTIES (${libtarget} PROPERTIES VERSION ${LIBSZIP_VERSION})
-    ENDIF (NOT CYGWIN)
-    SET_TARGET_PROPERTIES (${libtarget} PROPERTIES SOVERSION ${LIBSZIP_VERSION})
-  ENDIF (${libtype} MATCHES "SHARED")
+      set_target_properties (${libtarget} PROPERTIES VERSION ${LIBSZIP_VERSION})
+    endif (NOT CYGWIN)
+    set_target_properties (${libtarget} PROPERTIES SOVERSION ${LIBSZIP_VERSION})
+  endif (${libtype} MATCHES "SHARED")
 
   #-- Apple Specific install_name for libraries
-  IF (APPLE)
-    OPTION (SZIP_BUILD_WITH_INSTALL_NAME "Build with library install_name set to the installation path" OFF)
-    IF (SZIP_BUILD_WITH_INSTALL_NAME)
-      SET_TARGET_PROPERTIES(${libtarget} PROPERTIES
+  if (APPLE)
+    option (SZIP_BUILD_WITH_INSTALL_NAME "Build with library install_name set to the installation path" OFF)
+    if (SZIP_BUILD_WITH_INSTALL_NAME)
+      set_target_properties (${libtarget} PROPERTIES
           LINK_FLAGS "-current_version ${SZIP_PACKAGE_VERSION} -compatibility_version ${SZIP_PACKAGE_VERSION}"
           INSTALL_NAME_DIR "${CMAKE_INSTALL_PREFIX}/lib"
           BUILD_WITH_INSTALL_RPATH ${SZIP_BUILD_WITH_INSTALL_NAME}
       )
-    ENDIF (SZIP_BUILD_WITH_INSTALL_NAME)
-  ENDIF (APPLE)
+    endif (SZIP_BUILD_WITH_INSTALL_NAME)
+  endif (APPLE)
 
 ENDMACRO (SZIP_SET_LIB_OPTIONS)
 
