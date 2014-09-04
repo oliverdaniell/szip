2ef48e41a4f053abf51c40cf01cd6c7f9b174061
byrn <byrn@af834410-e117-0410-aeac-9316c7680557>
2014-09-04 18:24:04 +0000

    Add files needed for HDF Group CDash testing.
    git-svn-id: https://svn.hdfgroup.uiuc.edu/szip/trunk@156 af834410-e117-0410-aeac-9316c7680557

---
+++ config/cmake/cacheinit.cmake
@@ -0,0 +1,15 @@
+# This is the CMakeCache file.
+
+########################
+# EXTERNAL cache entries
+########################
+
+set (BUILD_SHARED_LIBS ON CACHE BOOL "Build Shared Libraries" FORCE)
+
+set (HDF_PACKAGE_EXT "" CACHE STRING "Name of package extension" FORCE)
+
+set (SZIP_BUILD_EXAMPLES ON CACHE BOOL "Build SZIP Library Examples" FORCE)
+
+set (BUILD_TESTING ON CACHE BOOL "Build SZIP Unit Testing" FORCE)
+
+set (SZIP_ENABLE_ENCODINGON CACHE BOOL "Enable SZIP Encoding" FORCE)
